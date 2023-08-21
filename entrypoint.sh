f39b0b351f73102e0315046c1c1bc9b2698eb42d
Tagscherer Ádám <adam.tagscherer@gmail.com>
2023-08-21 16:38:15 +0200

    feat(entrypoint): small changes in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
-#!/bin/ash
+#!/bin/sh
 
-USER_ID=$(id -u)
+set -euo pipefail
+
+export readonly USER_ID="$(id -u)"
 
 mkdir -p "$DB_ROOT_DIR/db"
 
-chown -R $USER_ID:$USER_ID $DB_ROOT_DIR
+chown -R "$USER_ID:$USER_ID" "$DB_ROOT_DIR"
 
 exec /app/grype-server "$@"
